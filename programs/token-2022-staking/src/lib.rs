e865218b74c581661cabd4e13dd4506d87e9250e
Saad Qayyum <saadqayyum794@gmail.com>
2024-06-30 00:33:17 +0500

    Implemented Initialize Method Successfully and Installed Next JS Project

--- programs/token-2022-staking/src/lib.rs
+++ programs/token-2022-staking/src/lib.rs
@@ -1,16 +1,81 @@
 use anchor_lang::prelude::*;
 
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{Mint, Token2022, TokenAccount},
+};
+
 declare_id!("GrpKuGPVTNjUCTqfJRMKpph7XZdfuBcc1cTLfRdSm3Xv");
 
 #[program]
 pub mod token_2022_staking {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    // 1. Initialize
+    pub fn initialize(ctx: Context<Initialize>, min_stake_period: i64) -> Result<()> {
+        let config = &mut ctx.accounts.config;
+
+        config.authority = *ctx.accounts.authority.key;
+        config.min_stake_period = min_stake_period;
+        config.reward_balance = 0;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        seeds = [CONFIG_PDA_SEED.as_ref()],
+        bump,
+        payer = authority,
+        space = 8 + 32 + 8 + 8
+    )]
+    pub config: Account<'info, Config>,
+
+    #[account(
+        init,
+        seeds = [CONFIG_ATA_SEED.as_ref()],
+        bump,
+        payer = authority,
+        token::mint = token_mint,
+        token::authority = config,
+        token::token_program = token_program,
+    )]
+    pub config_ata: Box<InterfaceAccount<'info, TokenAccount>>,
+
+    #[account(
+        mint::token_program = token_program
+    )]
+    pub token_mint: Box<InterfaceAccount<'info, Mint>>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token2022>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+// PDAs
+#[account]
+pub struct Config {
+    pub authority: Pubkey,     // 32
+    pub min_stake_period: i64, // 8
+    pub reward_balance: u64,   // 8
+}
+
+#[account]
+pub struct User {
+    pub authority: Pubkey,
+    pub stakes: Vec<Stake>,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+pub struct Stake {
+    pub amount: u64,
+    pub timestamp: i64,
+}
+
+pub const CONFIG_PDA_SEED: &[u8] = b"config-pda-1";
+pub const CONFIG_ATA_SEED: &[u8] = b"config-ata-1";
--- tests/token-2022-staking.ts
+++ tests/token-2022-staking.ts
@@ -2,15 +2,58 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Token2022Staking } from "../target/types/token_2022_staking";
 
+// -------------------------------------------------------------------------------------------------
+//                                GLOBAL VARIABLES
+// -------------------------------------------------------------------------------------------------
+
+const TOKEN_MINT = "x";
+const CONFIG_PDA_SEED = "config-pda-1";
+const CONFIG_ATA_SEED = "config-ata-1";
+
+const AUTHORITY_WALLET = [];
+
+let CONFIG_PDA = { configPda: null, configPdaBump: null };
+let CONFIG_ATA = { configAta: null, configAtaBump: null };
+
+// -------------------------------------------------------------------------------------------------
+
 describe("token-2022-staking", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(anchor.AnchorProvider.local());
 
   const program = anchor.workspace.Token2022Staking as Program<Token2022Staking>;
 
+  // -------------------------------------------------------------------------------------------------
+  //                                Setting Global Variables
+  // -------------------------------------------------------------------------------------------------
+
+  const TOKEN_MINT_PUBLIC_KEY = new anchor.web3.PublicKey(TOKEN_MINT);
+
+  const authorityWallet = anchor.web3.Keypair.fromSecretKey(Uint8Array.from(AUTHORITY_WALLET));
+  const authorityPublicKey = authorityWallet.publicKey;
+
+  [CONFIG_PDA.configPda, CONFIG_PDA.configPdaBump] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from(CONFIG_PDA_SEED)],
+    program.programId
+  );
+
+  [CONFIG_ATA.configAta, CONFIG_ATA.configAtaBump] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from(CONFIG_ATA_SEED)],
+    program.programId
+  );
+
+  // -------------------------------------------------------------------------------------------------
+  //                                        TESTS
+  // -------------------------------------------------------------------------------------------------
+
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+      .initialize(new anchor.BN(30))
+      .accounts({
+        authority: authorityPublicKey,
+        tokenMint: TOKEN_MINT_PUBLIC_KEY,
+      })
+      .signers([authorityWallet])
+      .rpc();
     console.log("Your transaction signature", tx);
   });
 });
